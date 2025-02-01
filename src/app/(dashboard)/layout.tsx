5ee980c4c9ec6e8bff92fd7d64ae95f54716a65e
Umair Qureshi <umairq96@gmail.com>
2025-02-01 11:20:41 -0800

    all list pages, table components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -17,13 +17,13 @@ export default function DashboardLayout({
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <Image src="/CA_LOGO.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">Cordoba</span>
+          <span className="hidden lg:block font-bold">Cordoba</span>
         </Link>
         <Menu />
       </div>
 
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>
